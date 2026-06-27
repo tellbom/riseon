e6955fec6235ac43351f8c5b0cefc261ed0f6f18
tellbom <tellbom@gmail.com>
2026-06-28 00:32:20 +0800

    Initial Commit

---
+++ RiseOn/RiseOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseOnApp.swift
+//  RiseOn
+//
+//  Created by 付子强 on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct RiseOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
